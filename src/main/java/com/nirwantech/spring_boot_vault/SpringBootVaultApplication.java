5d2439763d91144602dcd3991943cc0f4b35195f
gkishanrao <gkishanrao@gmail.com>
2025-08-19 11:48:55 -0700

    Initial commit

---
+++ src/main/java/com/nirwantech/spring_boot_vault/SpringBootVaultApplication.java
@@ -0,0 +1,14 @@
+package com.nirwantech.spring_boot_vault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootVaultApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootVaultApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nirwantech/spring_boot_vault/SpringBootVaultApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nirwantech.spring_boot_vault;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootVaultApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
