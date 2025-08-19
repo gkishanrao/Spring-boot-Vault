91f2f46634f031fe5c5c198dc04cf2eb9fb8d97f
gkishanrao <gkishanrao@gmail.com>
2025-08-19 11:54:03 -0700

    test

--- src/main/java/com/nirwantech/spring_boot_vault/SpringBootVaultApplication.java
+++ src/main/java/com/nirwantech/spring_boot_vault/SpringBootVaultApplication.java
@@ -6,9 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootVaultApplication {
 
-
-
-
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootVaultApplication.class, args);
 	}
